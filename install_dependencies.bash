99e4d455944465ae2afbb7b8f096cfec770d2343
Kristian Røste <github@roste.org>
2024-10-03 11:29:40 +0200

    added `install_dependencies.bash` for simplicity

---
+++ install_dependencies.bash
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+cd "$(dirname "$0")";
+git pull;
+curl -sS https://getcomposer.org/installer -o composer-setup.php && php composer-setup.php --install-dir=/usr/local/bin --filename=composer;
+composer self-update;
+composer install;
