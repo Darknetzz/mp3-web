7c3a7ce204ed2d9a7fb9b92401b6efdee7deddf3
Kristian Røste <github@roste.org>
2024-10-03 11:02:11 +0200

    removed unecessary js import

--- index.php
+++ index.php
@@ -58,8 +58,6 @@ function getDuration($file) {
 <script src="https://unpkg.com/dropzone@6.0.0-beta.1/dist/dropzone-min.js"></script>
 <link href="https://unpkg.com/dropzone@6.0.0-beta.1/dist/dropzone.css" rel="stylesheet" type="text/css" />
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jsmediatags/3.9.5/jsmediatags.min.js" integrity="sha512-YsR46MmyChktsyMMou+Bs74oCa/CDdwft7rJ5wlnmDzMj1mzqncsfJamEEf99Nk7IB0JpTMo5hS8rxB49FUktQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-
 <style>
   body {
     margin: 0;
