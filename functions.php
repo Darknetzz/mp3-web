d8c00bd72bc025497b25f3d69d634d0c9a2c3af4
Kristian <github@roste.org>
2025-12-27 20:49:24 +0100

    Refactor duration calculation in getDuration function for improved accuracy by explicitly casting seconds to integer and using floor for minutes.

--- functions.php
+++ functions.php
@@ -43,8 +43,8 @@
         }
         $mp3info  = new wapmorgan\Mp3Info\Mp3Info($file);
         $duration = $mp3info->duration;
-        $minutes  = floor($duration / 60);
-        $seconds  = (int)round($duration % 60);
+        $minutes  = (int)floor($duration / 60);
+        $seconds  = intval(round($duration % 60));
         $duration = sprintf("%d:%02d", $minutes, $seconds);
         return $duration;
     }
